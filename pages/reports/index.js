3b6888802dc843c45a7c588b364bf40eaa33d41f
Kelvin-Ben <kevoben14@gmail.com>
2024-04-20 13:33:29 +0300

    Import components into reports page

--- pages/reports/index.js
+++ pages/reports/index.js
@@ -1,8 +1,22 @@
+import { useRouter } from "next/router";
+
+import { getAllReports } from "@/dummy-data";
+import ReportList from "@/components/reports/report-list";
+import ReportSearch from "@/components/reports/report-search";
+import { Fragment } from "react";
 function Reports() {
+  const router = useRouter();
+  const allReports = getAllReports();
+  function findReportHandler(year, month) {
+    const fullPath = `/reports/${year}/${month}`;
+
+    router.push(fullPath);
+  }
   return (
-    <>
-      <h1>This is the reports page</h1>
-    </>
+    <Fragment>
+      <ReportSearch onSearch={findReportHandler} />
+      <ReportList items={allReports} />
+    </Fragment>
   );
 }
 export default Reports;
