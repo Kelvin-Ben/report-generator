42776e862b86ab07f1619d3515639ec2e1f602e5
Kelvin-Ben <kevoben14@gmail.com>
2024-04-20 13:39:54 +0300

    Create a component to filter specifi reports

--- pages/reports/[...slug].js
+++ pages/reports/[...slug].js
@@ -1,8 +1,61 @@
+import ReportList from "@/components/reports/report-list";
+import ReportTitle from "@/components/reports/report-title";
+import Button from "@/components/ui/button";
+import { getFilteredReports } from "@/dummy-data";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
 function FilteredReportsPage() {
+  const router = useRouter();
+
+  const filteredData = router.query.slug;
+  if (!filteredData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filteredData[0];
+  const filteredMonth = filteredData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2022 ||
+    (numMonth < 1) | (numMonth > 12)
+  ) {
+    return (
+      <Fragment>
+        <p className="alert">Invalid filter. Please adjust your values</p>
+        <div className="center">
+          <Button link="/reports">Show all Reports</Button>
+        </div>
+      </Fragment>
+    );
+  }
+  const filteredReport = getFilteredReports({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredReport || filteredReport.length === 0) {
+    return (
+      <Fragment>
+        <p className="alert">No report found for the chosen filter!</p>
+        <div className="center">
+          <Button link="/reports">Show all Reports</Button>
+        </div>
+      </Fragment>
+    );
+  }
+  const date = new Date(numYear, numMonth - 1);
   return (
-    <>
-    <h1>Filtered Reports Page</h1>
-    </>
-  )
+    <Fragment>
+      <ReportTitle date={date} />
+      <ReportList items={filteredReport} />
+    </Fragment>
+  );
 }
-export default FilteredReportsPage
+export default FilteredReportsPage;
