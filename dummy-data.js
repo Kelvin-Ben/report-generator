fff25d05974c2aeca609cfa30cd92c21d33b7849
Kelvin-Ben <kevoben14@gmail.com>
2024-04-20 13:32:49 +0300

    Modified dummy data and also the functions used withing the page

--- dummy-data.js
+++ dummy-data.js
@@ -3,50 +3,50 @@ const DUMMY_REPORTS = [
     id: "1",
     title: "Monthly Sales Report",
     date: "2024-04-01",
-    time: "08:00",
-    image: "sales_report_image.jpg",
+    time: "08:00 AM",
+    image: "images/download.svg",
   },
   {
     id: "2",
     title: "Expense Summary",
     date: "2024-04-05",
-    time: "09:30",
-    image: "expense_summary_image.jpg",
+    time: "09:30 AM",
+    image: "images/download.svg",
   },
   {
     id: "3",
     title: "Profit Analysis",
     date: "2024-04-10",
-    time: "11:15",
-    image: "profit_analysis_image.jpg",
+    time: "11:15 AM",
+    image: "images/download.svg",
   },
   {
     id: "4",
     title: "Quarterly Budget Review",
     date: "2024-03-20",
-    time: "10:45",
-    image: "budget_review_image.jpg",
+    time: "10:45 AM",
+    image: "images/download.svg",
   },
   {
     id: "5",
     title: "Yearly Financial Statement",
     date: "2024-02-15",
-    time: "14:00",
-    image: "financial_statement_image.jpg",
+    time: "02:00 PM",
+    image: "images/download.svg",
   },
   {
     id: "6",
     title: "Marketing Campaign Analysis",
     date: "2024-03-05",
-    time: "16:30",
-    image: "marketing_analysis_image.jpg",
+    time: "04:30 PM",
+    image: "images/download.svg",
   },
   {
     id: "7",
     title: "Annual Performance Review",
     date: "2024-02-10",
-    time: "13:45",
-    image: "performance_review_image.jpg",
+    time: "01:45 PM",
+    image: "images/download.svg",
   },
 ];
 
@@ -59,7 +59,7 @@ export function getFilteredReports(dateFilter) {
   let filteredReports = DUMMY_REPORTS.filter((report) => {
     const reportDate = new Date(report.date);
     return (
-      reportDate.getFullYear() === year && eventDate.getMonth() === month - 1
+      reportDate.getFullYear() === year && reportDate.getMonth() === month - 1
     );
   });
   return filteredReports;
