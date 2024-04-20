ea4e80c5e225f1c5c4cae94e88b7fd3f6e363026
Kelvin-Ben <kevoben14@gmail.com>
2024-04-20 13:39:08 +0300

    Modified the main page with global styles

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
 import { Inter } from "next/font/google";
 import { Fragment } from "react";
-import Reports from "./reports";
-import { getAllReports } from "@/dummy-data";
-import ReportList from "@/components/reports/report-list";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const allReports = getAllReports();
   return (
-    <>
-      <ReportList items={allReports} />
-    </>
+    <div className="alert">
+      <h1>This is the Home page</h1>
+    </div>
   );
 }
