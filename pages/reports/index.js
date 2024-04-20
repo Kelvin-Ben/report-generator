af7140b6e5349b432c36880674daf4cdf17ae8b3
Kelvin-Ben <kevoben14@gmail.com>
2024-04-20 18:40:41 +0300

    Add Pagination component to Reports page and implemented logic

--- pages/reports/index.js
+++ pages/reports/index.js
@@ -3,19 +3,38 @@ import { useRouter } from "next/router";
 import { getAllReports } from "@/dummy-data";
 import ReportList from "@/components/reports/report-list";
 import ReportSearch from "@/components/reports/report-search";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import Pagination from "@/components/pagination/Pagination";
+
 function Reports() {
   const router = useRouter();
+  const [currentPage, setCurrentPage] = useState(1);
+  const reportsPerPage = 3;
   const allReports = getAllReports();
+
   function findReportHandler(year, month) {
     const fullPath = `/reports/${year}/${month}`;
 
     router.push(fullPath);
   }
+  function handlePageChange(pageNumber) {
+    setCurrentPage(pageNumber);
+  }
   return (
     <Fragment>
       <ReportSearch onSearch={findReportHandler} />
-      <ReportList items={allReports} />
+      <ReportList
+        items={allReports.slice(
+          (currentPage - 1) * reportsPerPage,
+          currentPage * reportsPerPage
+        )}
+      />
+      <Pagination
+        totalReports={allReports.length}
+        reportsPerPage={reportsPerPage}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
     </Fragment>
   );
 }
